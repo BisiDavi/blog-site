98aaf3b5fc3662e576e281afd3317786fe1da2d3
BisiDavi <oludavidconnect@gmail.com>
2023-01-17 19:23:26 +0100

    search icon added

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function Searchbar() {
   return (
-    <form className="w-1/2">
+    <form className="w-1/2 flex relative">
+      <Image
+        src="/search.svg"
+        alt="search"
+        className="absolute ml-4"
+        height={40}
+        width={40}
+      />
       <input
         placeholder="Search for articles"
-        className="border bg-gray-100 border border-gray-200 rounded-xl w-full px-4 py-1.5"
+        className="border bg-gray-100 border border-gray-200  pl-20 rounded-xl w-full px-4 py-1.5"
       />
     </form>
   );
