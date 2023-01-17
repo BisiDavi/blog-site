e1478d391ea288e9b133ce65978c5c597c231da5
BisiDavi <oludavidconnect@gmail.com>
2023-01-17 21:32:29 +0100

    homepage updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,36 @@
-import { useEffect } from "react";
-
 import Layout from "@/layout";
-import { getAirtableData } from "@/utils/apiRequest";
+import { getAirtableBlogData } from "@/utils/apiRequest";
 
-export default function Home() {
-  useEffect(() => {
-    getAirtableData().then((response) => {
-      const result: any = [];
-      console.log("response", response.data);
-      response.data.records.map((item:any) => {
-        if (Object.values(item.fields).length !== 0) {
-          console.log("item", item);
-          result.push(item);
-        }
-      });
-      console.log("result", result);
-    });
-  }, []);
+interface Props {
+  articles: Array<{
+    id: string;
+    fields: {
+      "Blog Image": [];
+      Author: string;
+      tags: string[];
+      Blog: string;
+      Title: string;
+    };
+    createdTime: Date;
+  }>;
+}
+
+export default function Home({ articles }: Props) {
+  console.log("articles", articles);
 
   return (
     <Layout title="Reach More Audience by Writing and Getting Paid | Quill">
       <section></section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const result = await getAirtableBlogData();
+  console.log("result", result);
+  return {
+    props: {
+      articles: result,
+    },
+  };
 }
--- utils/apiRequest.ts
+++ utils/apiRequest.ts
@@ -1,13 +1,26 @@
 import axios from "axios";
 
-export async function getAirtableData() {
-  return await axios.get(
-    `https://api.airtable.com/v0/${process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID}/Table 1`,
-    {
-      params: { view: "Grid view" },
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_ACCESS_TOKEN}`,
-      },
-    }
-  );
+export async function getAirtableBlogData() {
+  return await axios
+    .get(
+      `https://api.airtable.com/v0/${process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID}/Table 1`,
+      {
+        params: { view: "Grid view" },
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_ACCESS_TOKEN}`,
+        },
+      }
+    )
+    .then((response) => {
+      const result: any = [];
+      console.log("response", response.data);
+      response.data.records.map((item: any) => {
+        if (Object.values(item.fields).length !== 0) {
+          console.log("item", item);
+          result.push(item);
+        }
+      });
+      console.log("result", result);
+      return result;
+    });
 }
